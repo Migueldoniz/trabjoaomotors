257a74a986872de2d1c25b95f2c0a8bae3aa0fb6
flavianitto <85723447+flavianitto@users.noreply.github.com>
2021-07-29 21:18:44 -0300

    Update Motocicleta.java

--- Controller/Motocicleta.java
+++ Controller/Motocicleta.java
@@ -1,10 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Controller;
+
 import java.io.*;
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
@@ -107,5 +102,4 @@ public class Motocicleta extends Veiculo{
         this.cadastrar();
         
     }
-       
 }

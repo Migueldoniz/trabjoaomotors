27453776295c06948623fa0b532b751e0e615056
Migueldoniz <70188765+Migueldoniz@users.noreply.github.com>
2021-07-30 18:18:53 -0300

    Update Pagamento.java

--- Controller/Pagamento.java
+++ Controller/Pagamento.java
@@ -1,15 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Controller;
 import javax.swing.JOptionPane;
 
 public class Pagamento {
-    private char pgtoTipo;
+    private String pgtoTipo;
     private double pgtoValor;
 
-    public char getPgtoTipo() {
+    public String getPgtoTipo() {
         return pgtoTipo;
     }
 
-    public void setPgtoTipo(char pgtoTipo) {
+    public void setPgtoTipo(String pgtoTipo) {
         this.pgtoTipo = pgtoTipo;
     }
 
@@ -21,35 +26,48 @@ public class Pagamento {
         this.pgtoValor = pgtoValor;
     }
 
-    public Pagamento(char pgtoTipo, double pgtoValor) {
+    public Pagamento(String pgtoTipo, double pgtoValor) {
         this.pgtoTipo = pgtoTipo;
         this.pgtoValor = pgtoValor;
     }
 
-    public double[] calculaValor(char pagtipo,double valor)
+    public double calculaValor(String pagtipo,double valor)
     {
         switch (pagtipo){
-            case 'V':
+            case "A vista":
             {
               double aux=valor;
-              double[] value = null;
-              value[0]=(valor+(aux*0.1));
+              double value;
+              value=(valor+(aux*0.1));
               return value;
             }
-            case 'P':
+            case "A prazo(12x)":
             {
              double aux=valor;
-             double[] valores = null;
-             valores[0]=(aux/12)*1.2;
-             valores[1]=(aux/24)*1.87;
-             valores[2]=(aux/36)*2.44;
-             return valores;
+             double valore;
+             valore=(aux/24)*1.87;
+             return valore;
+            }
+            case "A prazo(24x)":
+            {
+             double aux=valor;
+             double valoree;
+             valoree=(aux/12)*1.2;
+             return valoree;
+            }
+            
+            case "A prazo(36x)":
+            {
+             double aux=valor;
+             double valoreeee;
+             valoreeee=(aux/36)*2.44;
+             return valoreeee;
             }
             default:
             {
                 JOptionPane.showMessageDialog(null,"Forma de pagamento errônea","Erro!", JOptionPane.ERROR_MESSAGE);
             }
         }
-        return null;
+        return 0;
     }
 }

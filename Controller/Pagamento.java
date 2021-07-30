07f34660f57986e0f0a022f24aa5213dd65c84de
Migueldoniz <70188765+Migueldoniz@users.noreply.github.com>
2021-07-29 22:14:38 -0300

    Update Pagamento.java

--- Controller/Pagamento.java
+++ Controller/Pagamento.java
@@ -1,14 +1,15 @@
 package Controller;
+import javax.swing.JOptionPane;
 
 public class Pagamento {
-    private String pgtoTipo;
+    private char pgtoTipo;
     private double pgtoValor;
 
-    public String getPgtoTipo() {
+    public char getPgtoTipo() {
         return pgtoTipo;
     }
 
-    public void setPgtoTipo(String pgtoTipo) {
+    public void setPgtoTipo(char pgtoTipo) {
         this.pgtoTipo = pgtoTipo;
     }
 
@@ -20,15 +21,35 @@ public class Pagamento {
         this.pgtoValor = pgtoValor;
     }
 
-    public Pagamento(String pgtoTipo, double pgtoValor) {
+    public Pagamento(char pgtoTipo, double pgtoValor) {
         this.pgtoTipo = pgtoTipo;
         this.pgtoValor = pgtoValor;
     }
 
-    public double calculaValor(double valor)
+    public double[] calculaValor(char pagtipo,double valor)
     {
-        double soma=0;
-        soma+=valor;
-        return soma;
+        switch (pagtipo){
+            case 'V':
+            {
+              double aux=valor;
+              double[] value = null;
+              value[0]=(valor+(aux*0.1));
+              return value;
+            }
+            case 'P':
+            {
+             double aux=valor;
+             double[] valores = null;
+             valores[0]=(aux/12)*1.2;
+             valores[1]=(aux/24)*1.87;
+             valores[2]=(aux/36)*2.44;
+             return valores;
+            }
+            default:
+            {
+                JOptionPane.showMessageDialog(null,"Forma de pagamento errônea","Erro!", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        return null;
     }
 }

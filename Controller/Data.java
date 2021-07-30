d7dab5a7d583360595a62f716838c1a30c7a6bec
flavianitto <85723447+flavianitto@users.noreply.github.com>
2021-07-29 21:35:03 -0300

    Update Data.java

--- Controller/Data.java
+++ Controller/Data.java
@@ -1,29 +1,26 @@
 package Controller;
 
-public class Data
+import javax.swing.JOptionPane;
+
+public class Hora
 {
-    private String dataFormatada;
+    private String horaFormatada;
     private String[] aux;
-    private int dia;
-    private int mes;
-    private int ano;
-
-    public void setData(String nometext)
-    {
-        dataFormatada=nometext;
-        aux=dataFormatada.split("/");
-        dia=Integer.parseInt(aux[0]);
-        mes=Integer.parseInt(aux[1]);
-        ano=Integer.parseInt(aux[2]);
-        if((dia > 0 && dia < 32) && (mes > 0 && mes < 13) && (ano > 1900 && ano < 2022))
-            this.dataFormatada = dia + "/" + mes + "/" + ano;
+    private int hora;
+    private int minuto;
+    public void setHora(String nometext)
+    {   
+        horaFormatada=nometext;
+        aux=horaFormatada.split(":");
+        hora=Integer.parseInt(aux[0]);
+        minuto=Integer.parseInt(aux[1]);
+        if((hora > -1 && hora < 24) && (minuto > -1 && minuto < 60))
+            this.horaFormatada = hora + ":" + minuto;
         else
-            System.out.println("Data invalida!");
+            JOptionPane.showMessageDialog(null, "Horário inválido!", "Erro", JOptionPane.ERROR_MESSAGE);
     }
-
-    public String getData()
+    public String getHora()
     {
-        return this.dataFormatada;
+        return this.horaFormatada;
     }
-	
 }
